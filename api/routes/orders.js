91aa00c5f67497206458dc1957829ed1142f20ad
vivekmalumel <vivek.kply@gmail.com>
2019-07-30 01:34:11 +0530

    Product and Order added

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,23 +1,93 @@
 const router=require('express').Router();
+const mongoose=require('mongoose');
+const Order=require('../models/order');
+const Product=require('../models/product')
 
-router.get('/',(req,res)=>{
-    res.status(200).json({
-        message:"Get All Orders"
-    })
+router.get('/',async (req,res)=>{
+    try {
+        const result=await Order.find().select("_id product quantity");
+        const response={
+            count:result.length,
+            orders:result.map(doc=>{
+                const {_id,product,quantity}=doc;
+                return{
+                    _id,
+                    product,
+                    quantity,
+                    request:{
+                        type:'GET',
+                        url:'http://localhost:5000/api/orders/'+_id
+                    }
+                }
+            })
+        }
+        res.status(200).json(response)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+    
 });
 
-router.get('/:id',(req,res)=>{
-    const id=req.params.id;
-    res.status(200).json({
-        id
+router.post('/',async (req,res)=>{
+    
+    const order=new Order({
+        _id:new mongoose.Types.ObjectId(),
+        quantity:req.body.quantity,
+        product:req.body.productId
     })
+    try {
+        const prd=await Product.findById(order.product);
+        //console.log(prd)
+        if(!prd)
+            return res.status(400).json({message:"Product Not found"});
+        const result=await order.save();
+        const {_id,product,quantity}=result;
+        const response={
+            message:"Order Created",
+            order:{
+                _id,
+                product,
+                quantity,
+                request:{
+                    type:"GET",
+                    url:"http://localhost:5000/api/orders/"+_id
+                }
+            }
+        }
+        res.status(201).json(response)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
 })
 
-router.delete('/:id',(req,res)=>{
+router.get('/:id',async (req,res)=>{
     const id=req.params.id;
-    res.status(200).json({
-        message:"Order Deleted"
-    })
+    try {
+        const result=await Order.findById(id).select("_id quantity product");
+        if(result)
+        res.status(200).json(result);
+        else
+            res.status(404).json({message:"No valid Entry Found"});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+})
+
+router.delete('/:id',async (req,res)=>{
+    const id=req.params.id;
+    try {
+        const result=await Order.findByIdAndDelete(id).select("_id quantity product");
+        if(result)
+            res.status(200).json(result);
+        else
+            res.status(404).json({message:"No valid Entry Found"});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
 })
 
 module.exports=router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -4,8 +4,20 @@ const Product=require('../models/product');
 
 router.get('/',async(req,res)=>{
     try {
-        const result =await Product.find().exec();
-        res.status(200).json(result);
+        const result =await Product.find().select('_id name price');
+        const response={
+            count:result.length,
+            products:result.map(doc=>{
+                const{_id,name,price}=doc;
+                return{
+                    _id,
+                    name,
+                    price,
+                    request:{type:'GET',url:"http://localhost:5000/api/products/"+_id}
+                }
+            })
+        }
+        res.status(200).json(response);
     } catch (error) {
         console.log(error);
         res.status(500).json({error});
@@ -19,8 +31,13 @@ router.post('/',async (req,res)=>{
         price:req.body.price
     })
     try {
-        const result=await product.save()
-        res.status(201).json(result)
+        const result=await product.save();
+        const {_id,name,price}=result
+        const response={
+            message:"Product created successfully",
+            product:{_id,name,price,request:{type:'GET',url:"http://localhost:5000/api/products/"+_id}}
+        }
+        res.status(201).json(response)
     } catch (error) {
         console.log(error);
         res.status(500).json({error});
@@ -32,8 +49,15 @@ router.post('/',async (req,res)=>{
 router.get('/:id',async (req,res)=>{
     const id=req.params.id;
     try {
-        const result =await Product.findById(id);
-        res.status(200).json(result);
+        const result =await Product.findById(id).select("_id name price");
+        if(result){
+           // const {_id,name,price}=result;
+            res.status(200).json(result);
+        }
+        else{
+            res.status(404).json({message:"No valid Entry Found"});
+        }
+        
     } catch (error) {
         console.log(error);
         res.status(500).json({error});
@@ -45,7 +69,8 @@ router.delete('/:id',async (req,res)=>{
     try {
         const result =await Product.findByIdAndDelete(id);
         if(result){
-            res.status(200).json(result);
+            const {_id,name,price}=result
+            res.status(200).json({_id,name,price});
         }
         else{
             res.status(404).json({message:"No valid Entry Found"});
@@ -67,7 +92,7 @@ router.patch('/:id',async (req,res)=>{
     console.log(updateOps);
     try {
         const result =await Product.update({_id:id},{$set:updateOps});
-        if(result){
+        if(result.n){
             res.status(200).json(result);
         }
         else{
